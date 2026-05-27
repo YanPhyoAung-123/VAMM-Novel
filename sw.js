74c1a647b224880ea22a0dc49159a0f70cd1a14a
YanPhyoAung-123 <phyoaungyan35@gmail.com>
2026-05-28 00:09:40 +0630

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,4 @@
-const cacheName = 'vamm-v1';
-const filesToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/login.html',
-  '/register.html',
-  '/detail.html',
-  '/profile.html',
-  '/how-to-buy.html',
-  '/buy-coin.html',
-  '/support.html'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+  // ဒါက ဘာမှ မလုပ်ဘဲ Website ကို တိုက်ရိုက်သွားခိုင်းတာပါ။
+  // ဒါဆိုရင် PWA ဖြစ်ဖို့ လိုအပ်ချက် ပြည့်သွားပါပြီ။
 });
